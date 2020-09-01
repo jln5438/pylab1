cf5b4f35699c33cfa97049788d00a12372e26ffd
jln5438 <jln5438@psu.edu>
2020-09-01 16:46:41 +0000

    made my print statements the correct form.

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ temp=float(temp)
 unit=input("Enter unit in F/f or C/c: ")
 if(unit=='f' or unit=='F'):
   ctemp=(temp-32)*5/9
-  print(str(temp)+"° in Fahrenheit is equivalent to "+str(ctemp)+"° Celsius.")
+  print(f"{temp}° in Fahrenheit is equivalent to {ctemp}° Celsius.")
 elif(unit=='c' or unit =='C'):
   ftemp=(temp*9/5)+32
-  print(str(temp)+"° in Celsius is equivalent to "+str(ftemp)+"° Fahrenheit.")
+  print(f"{temp}° in Celsius is equivalent to {ftemp}° Fahrenheit.")
 else:
   print("Invalid unit("+unit+").")
