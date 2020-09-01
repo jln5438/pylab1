774cdd711f6e4174904b511506e9b10e50558a8b
jln5438 <jln5438@psu.edu>
2020-09-01 15:45:55 +0000

    capitalization

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 temp=input("Enter temperature: ")
 unit=input("Enter unit in F/f or C/c: ")
 if(unit=='f' or unit=='F'):
-  temp=(temp-32)*5/9
-elif(unit=='c' or unit ='C'):
-   
+  ctemp=(temp-32)*5/9
+  print(temp+"° in Fahrenheit is equivalent to "+ctemp+"° Celsius.")
+elif(unit=='c' or unit =='C'):
+  ftemp=(temp*9/5)+32
+  print(temp+"° in Celsius is equivalent to "+ftemp+"° Fahrenheit.")
+else:
+  print("Invalid unit("+unit+")")
